018d02d5ae48ca02dcbfab38c858b513c7babaea
unknown <rajaafzalkhan123456789@gmail.com>
2026-02-17 09:15:32 +0000

    Initial fullstack setup

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
